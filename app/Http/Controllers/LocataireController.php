2452ef55063269e31d329e1cd50dd2059be544e7
winner40 <vanecktiyo0@gmail.com>
2023-06-26 11:32:58 +0100

    creation de la migration locataire

---
+++ app/Http/Controllers/LocataireController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Locataire;
+use App\Http\Requests\StoreLocataireRequest;
+use App\Http\Requests\UpdateLocataireRequest;
+
+class LocataireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLocataireRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Locataire $locataire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Locataire $locataire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLocataireRequest $request, Locataire $locataire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Locataire $locataire)
+    {
+        //
+    }
+}
