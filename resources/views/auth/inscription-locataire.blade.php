6ab27d5520d443694cc9c2c56eca24705442cc3d
David4Lesage <david4lesage@gmail.com>
2023-07-12 20:45:54 +0100

    Responsivité du dashboard Propriétaire

--- resources/views/auth/inscription-locataire.blade.php
+++ resources/views/auth/inscription-locataire.blade.php
@@ -139,10 +139,6 @@
 
 
     @include('commun.footer')
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <script>
         const togglePassword = document.querySelector('#togglePassword');
