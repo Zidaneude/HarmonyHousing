c46d955015256178cb5a84ef56552451bff13666
David4Lesage <david4lesage@gmail.com>
2023-07-12 17:01:26 +0100

    Footer Responsive

--- resources/views/commun/footer.blade.php
+++ resources/views/commun/footer.blade.php
@@ -1,42 +1,26 @@
 <footer class="container-fluid">
     <div class="container py-4">
         <div class="row">
-            <div class="col-3">
+            <div class="col-12 col-md-3 order-1 order-md-1">
                 <a href="#"> <img class="img-fluid" style="height: 40px;" src="images/Logo_Transparent.png"
                         alt="Logo Harmony Housing"></a>
-            </div>
-            <div class="col-3">
-                <h5>En savoir plus !</h5>
-            </div>
-            <div class="col-2">
-                <h5>Trouver un logement</h5>
-            </div>
-            <div class="col-2">
-                <h5>Rejoignez-nous !</h5>
-            </div>
-            <div class="col-2 app-column">
-                <h5>Application</h5>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-3">
                 <ul class="list-unstyled text-small">
                     <li><a class="text-muted" href="#">Espace propriétaire</a></li>
                     <li><a class="text-muted" href="/comment-ca-marche">Comment ça marche ?</a></li>
                     <li><a class="text-muted" href="/connexion-locataire">Connexion</a></li>
                 </ul>
             </div>
-            <div class="col-3">
+            <div class="col-12 col-md-3 order-2 order-md-2">
+                <h5>En savoir plus !</h5>
                 <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Politique de confidentialité</a></li>
                     <li><a class="text-muted" href="#">CGU</a></li>
                     <li><a class="text-muted" href="/mension-legale">Mentions légales</a></li>
                     <li><a class="text-muted" href="/FAQ-Locataire">FAQ - Locataire</a></li>
                     <li><a class="text-muted" href="/FAQ-Proprietaire">FAQ - Propriétaire</a></li>
                 </ul>
             </div>
-            <div class="col-2">
+            <div class="col-12 col-md-2 order-3 order-md-3">
+                <h5>Trouver un logement</h5>
                 <ul class="list-unstyled text-small">
                     <li><a class="text-muted" href="#">Logement à Douala</a></li>
                     <li><a class="text-muted" href="#">Logement à Yaoundé</a></li>
@@ -45,9 +29,10 @@
                     <li><a class="text-muted" href="#">Location à Bertoua</a></li>
                 </ul>
             </div>
-            <div class="col-2">
+            <div class="col-12 col-md-2 order-4 order-md-4">
+                <h5>Rejoignez-nous !</h5>
                 <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="/contactez-nous">Nous contacter</a></li>
+                    <li><a class="text-muted" href="#">Nous contacter</a></li>
                 </ul>
                 <ul class="list-inline">
                     <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
@@ -57,12 +42,12 @@
                 </ul>
             </div>
 
-            <div class="col-2">
+            <div class="col-12 col-md-2 order-5 order-md-5">
+                <h5>Application</h5>
                 <ul class="list-unstyled text-small">
                     <li><a class="text-muted" href="#">Télécharger</a></li>
                 </ul>
             </div>
         </div>
-
     </div>
 </footer>
