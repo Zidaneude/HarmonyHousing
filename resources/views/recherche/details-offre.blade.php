9ede1d5c71a526860807172fd1db2e8ef50eb895
Zidaneude <nawessizidane@gmail.com>
2023-07-14 15:20:06 +0100

    mise a jour

--- resources/views/recherche/details-offre.blade.php
+++ resources/views/recherche/details-offre.blade.php
@@ -2,6 +2,14 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v5.8.14, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
+
+    <meta name="description" content="">
+
     <title>Détails du logement - Harmony Housing</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,15 +19,18 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
 </head>
 
-<body >
+<body>
     @include('commun.header')
     <section data-bs-version="5.1" class="gallery6 mbr-gallery cid-tIWkkBEfM2" id="gallery6-1l">
+
         <div class="container-fluid">
+
             <div class="row mbr-gallery mt-4">
                 <div class="col-12 col-md-6 col-lg-4 item gallery-image">
                     <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#tIWCMqyrrd-modal"
                         data-bs-target="#tIWCMqyrrd-modal">
-                        <img class="w-100" src="{{ asset('assets/images/hilton-yaounde-habitacion-c4dc94a-1000x615.jpg') }}"
+                        <img class="w-100"
+                            src="{{ asset('assets/images/hilton-yaounde-habitacion-c4dc94a-1000x615.jpg') }}"
                             alt="Mobirise Website Builder" data-slide-to="0" data-bs-slide-to="0"
                             data-target="#lb-tIWCMqyrrd" data-bs-target="#lb-tIWCMqyrrd">
                         <div class="icon-wrapper">
@@ -31,7 +42,8 @@
                 <div class="col-12 col-md-6 col-lg-4 item gallery-image">
                     <div class="item-wrapper" data-toggle="modal" data-bs-toggle="modal" data-target="#tIWCMqyrrd-modal"
                         data-bs-target="#tIWCMqyrrd-modal">
-                        <img class="w-100" src="{{ asset('assets/images/hotel-la-falaise-yaounde-habitacion-ba97e4e-500x333.jpg') }}"
+                        <img class="w-100"
+                            src="{{ asset('assets/images/hotel-la-falaise-yaounde-habitacion-ba97e4e-500x333.jpg') }}"
                             alt="Mobirise Website Builder" data-slide-to="1" data-bs-slide-to="1"
                             data-target="#lb-tIWCMqyrrd" data-bs-target="#lb-tIWCMqyrrd">
                         <div class="icon-wrapper">
@@ -59,8 +71,7 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-body">
-                            <div class="carousel slide" id="lb-tIWCMqyrrd" data-interval="5000"
-                                data-bs-interval="5000">
+                            <div class="carousel slide" id="lb-tIWCMqyrrd" data-interval="5000" data-bs-interval="5000">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
                                         <img class="d-block w-100"
@@ -108,7 +119,408 @@
             </div>
         </div>
     </section>
-    @include('commun/footer')
+
+
+
+    <section data-bs-version="5.1" class="features12 cid-tIyP0hsNzW" id="features13-10">
+
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Accueil</a></li>
+                    <li class="breadcrumb-item"><a href="/">Mon dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="/">Mes annonces</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Détails de l'annonce</a></li>
+
+                </ol>
+            </nav>
+        </div>
+        <style>
+            ul.breadcrumb {
+                width: auto;
+                padding: 10px 315px;
+                list-style: none;
+                background-color: #eee;
+            }
+
+            ul.breadcrumb li {
+                display: inline;
+                font-size: 18px;
+            }
+
+            ul.breadcrumb li+li:before {
+                padding: 8px;
+                color: black;
+                content: ">\00a0";
+            }
+
+            ul.breadcrumb li a {
+                color: #004aad;
+                text-decoration: none;
+                font-family: poppins;
+                text-align: center;
+            }
+
+            ul.breadcrumb li a:hover {
+                color: #01447e;
+                text-decoration: solid;
+            }
+        </style>
+
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div class="card-wrapper">
+                        <div class="card-box align-left">
+                            <h3 class="card-title mbr-fonts-style mb-4 display-7" style="font-size: 22px;">
+                                <strong>Appartement meublé Face à
+                                    Santa Lucia Ngousso-Yaoundé</strong>
+                            </h3>
+                            <h5 class="card-subtitle"><strong>Equipements du logement</strong></h5>
+                            <style>
+                                .card-subtitle {
+                                    color: black;
+                                    padding-top: 5px;
+                                    padding-bottom: 20px;
+                                    background-color: #fcfbfb;
+                                    font-family: poppins;
+
+                                }
+                            </style>
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="features12 cid-tIEPMOJNFj" id="features13-1e">
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-6">
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" checked disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mbrib-wifi"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">
+                                internet</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mbrib-desktop"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Ordinateur</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" checked disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mobi-mbri-video-play mobi-mbri"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">TV</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mobi-mbri-website-theme mobi-mbri"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Cintres pour vetements</h4>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" checked disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont socicon-curse socicon"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Chaufage</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" checked disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mobi-mbri-mobirise mobi-mbri"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Climatisation</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" checked disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mbrib-protect"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Equipements d'hygiène</h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check1" name="option1"
+                                value="something" disabled>
+                        </div>
+                        <div class="icon-box">
+                            <span class="mbr-iconfont mbrib-success"
+                                style="color: rgb(0, 0, 0); fill: rgb(0, 0, 0);"></span>
+                        </div>
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Fer à repasser</h4>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </section><br>
+
+    <section data-bs-version="5.1" class="features12 cid-tIEPMOJNFj" id="features13-1e">
+
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-12 col-lg-6">
+
+                    <div class="card">
+                        <div class="card-wrapper">
+                            <div class="row align-items-center">
+                                <div class="col-12 col-md-4">
+                                    <div class="image-wrapper">
+                                        <a href="détails et reservation.html"><img height="200"
+                                                style="border-radius: 5px;"
+                                                src="assets/images/502d0f906d18e6e1568af5e39875b5f9-816x461.jpg"
+                                                alt="Mobirise Website Builder"></a>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md">
+                                    <div class="card-box">
+                                        <div class="row">
+                                            <div class="col-md">
+                                                <h6 class="card-title mbr-fonts-style display-7"><strong>Chambre 01
+                                                    </strong><br></h6>
+                                                <p class="mbr-text mbr-fonts-style display-7"
+                                                    style="margin-left: 0px;">
+                                                    Meublé<br>
+                                                    Penderie<br>
+                                                    douche interne
+                                                </p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><br>
+                </div>
+                <div class="col-12 col-lg-6">
+
+
+                    <div class="card">
+                        <div class="card-wrapper">
+                            <div class="row align-items-center">
+                                <div class="col-12 col-md-4">
+                                    <div class="image-wrapper">
+                                        <a href="détails et reservation.html"><img height="200"
+                                                style="border-radius: 5px;"
+                                                src="assets/images/hilton-yaounde-550x363.jpg"
+                                                alt="Mobirise Website Builder"></a>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md">
+                                    <div class="card-box">
+                                        <div class="row">
+                                            <div class="col-md">
+                                                <h6 class="card-title mbr-fonts-style display-7"><strong>Chambre 02
+                                                    </strong><br></h6>
+                                                <p class="mbr-text mbr-fonts-style display-7">
+                                                    vide<br>
+
+                                                </p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="features12 cid-tIEPMOJNFj" id="features13-1e">
+
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-12 col-lg-6">
+                    <div class="col-12 col-lg-6">
+                        <p class="mbr-text mbr-fonts-style mb-4 display-5"><strong>75000 FCFA/mois</strong></p>
+
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Nombre de chambres dans
+                                l'appartement: <strong>02</strong></h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Disponibilité:
+                                <strong>Actuellement disponible</strong>
+                            </h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Fréquence de paiement: <strong>6
+                                    mois</strong></h4>
+
+                        </div>
+                    </div>
+                    <div class="item mbr-flex">
+                        <div class="text-box">
+                            <h4 class="icon-title mbr-black mbr-fonts-style display-7">Localisation:
+                                <strong>Youndé-Ngousso,A 100m de Santa Lucia</strong>
+                            </h4>
+
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-12 col-lg-6">
+
+
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+
+    <style>
+        .container2 {
+            font-family: poppins;
+            text-align: center;
+            padding: auto;
+            height: 500px;
+        }
+
+        .form-group {
+            border-color: #091729;
+            width: 750px;
+            margin-left: 550px;
+        }
+
+        .tit {
+            padding-top: 20px;
+        }
+
+        .note {
+            padding-top: 15px;
+            font-family: poppins;
+            color: white;
+        }
+
+        .avis {
+            padding-top: 15px;
+            font-family: poppins;
+            color: white;
+        }
+
+        .rating {
+            direction: rtl;
+            text-align: center;
+        }
+
+        .rating a {
+            color: #acafae;
+            text-decoration: none;
+            font-size: 3em;
+            transition: color .4s;
+        }
+
+        .rating a:hover,
+        .rating a:focus,
+        .rating a:hover~a,
+        .rating a:focus~a {
+            color: orange;
+            cursor: pointer;
+        }
+    </style>
+    </section>
+
+
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/ytplayer/index.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+    <script src="assets/formoid/formoid.min.js"></script>
+
+
 </body>
 
 </html>
