c96034aa931f61260c413e661733f10c2d92f906
winner40 <vanecktiyo0@gmail.com>
2023-07-15 13:26:05 +0100

    rien

--- app/Http/Controllers/Auth/Locataire/EmailVerificationNotificationLocataireController.php
+++ app/Http/Controllers/Auth/Locataire/EmailVerificationNotificationLocataireController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Auth\Locataire;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
-class EmailVerificationNotificationLocataireController extends Controller
+class EmailVerificationNotificationController extends Controller
 {
-    //
+    /**
+     * Send a new email verification notification.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('status', 'verification-link-sent');
+    }
 }
